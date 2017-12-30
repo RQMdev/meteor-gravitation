7fabc28b60f9f0ddd9c1931e5bcb6405acb67ba5
Benoit Laurent <benoit.jacques.felix@gmail.com>
2017-12-30 12:45:58 +0100

    add Error displaying component

---
+++ imports/ui/components/DisplayError/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DisplayError = message => (
+  <div className="error">{{message}}</div>
+);
+
+export default DisplayError
--- imports/ui/pages/SignUp/index.js
+++ imports/ui/pages/SignUp/index.js
@@ -2,30 +2,44 @@ import React from 'react'
 import { Accounts } from 'meteor/accounts-base'
 
 import Input from '../../components/Input'
+import DisplayError from '../../components/DisplayError'
 
 export default class SignUp extends React.Component {
   constructor(props){
     super(props)
+    this.state = {
+      error: ''
+    }
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit(e){
     e.preventDefault();
     const equalPwd = this.state.password === this.state.confirmation
-    if (!equalPwd) return;
+    if (!equalPwd) return this.setState({error: "Passwords don't match."})
     console.log(this.state)
     const options = {
       username: this.state.username,
       email: this.state.email,
       password: this.state.password
     }
-    const callback = (error) => console.error(error)
+    const callback = (error) => {
+      if (error){
+        this.setState({error: error.reason});
+      }
+    }
     Accounts.createUser(options, callback);
   }
 
   render() {
+    const displayError = '';
+    if (this.state.error){
+      displayError = <DisplayError message={this.state.error} />
+    }
+
     return (
       <form onSubmit={this.handleSubmit}>
+        {displayError}
         <Input
           id="username"
           type="text"
