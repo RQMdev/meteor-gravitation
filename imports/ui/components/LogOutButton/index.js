a99f7b17fd486d955a0b2907dc1ae7ce13e30860
Benoit Laurent <benoit.jacques.felix@gmail.com>
2017-12-31 15:37:51 +0100

    refactor logOutButton

--- imports/ui/components/LogOutButton/index.js
+++ imports/ui/components/LogOutButton/index.js
@@ -4,12 +4,7 @@ import { AuthService } from '../../../services/AuthService'
 import { styles } from './styles'
 
 const LogOutButton = () => {
-  const logOut = () => {
-    Meteor.logout(AuthService.callback)
-  }
-  return (
-    <button style={styles} onClick={logOut}>Log Out</button>
-  )
+  return <button style={styles} onClick={Meteor.logout}>Log Out</button>
 }
 
 export default LogOutButton
